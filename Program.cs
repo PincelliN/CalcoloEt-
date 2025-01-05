2cabe3a01f27168071174dcd25c7ecf333b5037b
Nicolò <pincelli.n@gmail.com>
2025-01-05 17:39:52 +0100

    add getAnni

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Inserisci la tua data di nascita");
+string dateOfBirth = Console.ReadLine();
+
+static int getAnni(DateTime dataDiNascità, DateTime today)
+{
+    return today.Month > dataDiNascità.Month ? today.Year - dataDiNascità.Year : (today.Year - dataDiNascità.Year) - 1;
+}
+
+bool input = DateTime.TryParse(dateOfBirth, out DateTime dataDiNascità);
+DateTime today = DateTime.Today;
+Console.WriteLine(today);
+if (input)
+{
+    int age = getAnni(dataDiNascità, today);
+    Console.WriteLine(age);
+}
